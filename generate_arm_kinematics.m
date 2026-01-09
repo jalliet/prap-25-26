bf41b2f3cb3fac25e30d2edec42f5e82d735834e
FHL-08 <faisallawan08@gmail.com>
2026-01-09 05:51:45 +0000

    Fixed bugs in Inverse Kinematics Algorithm

--- generate_arm_kinematics.m
+++ generate_arm_kinematics.m
@@ -20,14 +20,13 @@ l_2 = 0.07;
 l_3 = 0.03;
 l_4 = 0.115;
 l_5 = 0.03;
-l_6 = 0.105;
+l_6 = 0.137;
 l_7 = 0.0045;
-l_8 = 0.032;
-l_9 = 0.06;
-l_10 = 0.03;
-l_11 = 0.02;
+l_8 = 0.06;
+l_9 = 0.03;
+l_10 = 0.02;
+l_11 = 0.055;
 l_12 = 0.055;
-l_13 = 0.055;
 
 %% Robot Forward Kinematics (Denavit-Hartenberg Parameters)
 % Defined for the ARM project's modified Hugging Face robot
@@ -41,13 +40,12 @@ dh_table = [  0,           0,      x_0,     0;     % WtoP1
               0,       -pi/2,      l_5,     0;     % 2toP3
               0,        pi/2,      l_6,     0;     % P3to3
               0,       -pi/2,      l_7,     0;     % 3toP4
-              0,        pi/2,      l_8,     0;     % P4toP4'
-              0,           0,      l_9,     0;     % P4'to4
+              0,        pi/2,      l_8,     0;     % P4to4
               0,       -pi/2,        0, -pi/2;     % 4toP5
               0,           0,        0,     0;     % P5to5
-           l_10,           0,     l_11,  pi/2;     % 5toP6
-              0,        pi/2,     l_12,     0;     % P6to6
-              0,        pi/2,     l_13,     0;     % 6toalign1
+            l_9,           0,     l_10,  pi/2;     % 5toP6
+              0,        pi/2,     l_11,     0;     % P6to6
+              0,        pi/2,     l_12,     0;     % 6toalign1
               0,       -pi/2,        0,  pi/2;     % align1toalign2
               0,       -pi/2,        0,     0;     % align2toEE
            ];
@@ -57,7 +55,7 @@ H = eye(4);
 H_init = eye(4);
 
 joint_ID = 1;
-joint_info = [3, 5, 7, 10, 12, 14];
+joint_info = [3, 5, 7, 9, 11, 13];
 init_joint_positions = cell(n_joints, 1);
 joint_positions = cell(n_joints, 1);
 joint_rotation_axes = cell(n_joints, 1);
@@ -132,6 +130,7 @@ cartesian_state = [world_frame_EE_position; world_frame_EE_orientation];
 FK_func = Function('FK_func', {q}, {cartesian_state});
 % Example usage: 
 % cart_state = full(FK_func([0.3; -0.2; 0; 0.2; 0.2; 0.15]));
+% 0.3; 0.2; 0; 0.1; 0.2
 % NB: make sure q4 is defined such that it is parallel to the xy-plane,
 % else you will get errors in the inverse kinematics.
 
@@ -214,29 +213,69 @@ world_frame_wrist_position = p_EE + R_EE*EE_frame_wrist_position;
 p_1_to_wrist_baseFrame = world_to_base*[world_frame_wrist_position; 1];
 IK(1) = atan2(p_1_to_wrist_baseFrame(2), p_1_to_wrist_baseFrame(1));
 
-p_2_to_3_worldFrame = init_joint_positions{3} - init_joint_positions{2};
-p_3_to_4_worldFrame = init_joint_positions{4} - init_joint_positions{3};
+% Extract vectors for the arm at rest (q=0) using initial FK positions
+% These are numerical values calculated during the setup loop
+p2_0 = init_joint_positions{2};
+p3_0 = init_joint_positions{3};
+p4_0 = init_joint_positions{4};
 
-lambda_1 = norm(p_2_to_3_worldFrame);
-lambda_2 = norm(p_3_to_4_worldFrame);
+vec_Upper_0 = p3_0 - p2_0; % Vector P2->P3 at rest
+vec_Fore_0  = p4_0 - p3_0; % Vector P3->P4 at rest
 
-beta = acos((p_2_to_3_worldFrame'*frame_2_x_axis)/lambda_1);
-gamma = acos((p_3_to_4_worldFrame'*frame_3_x_axis)/lambda_2);
+% Calculate Link Lengths (Constants)
+lambda_1 = norm(vec_Upper_0);
+lambda_2 = norm(vec_Fore_0);
 
-delta_0 = (pi/2 - beta) + gamma + pi/2;
+% Calculate the "Reference" Elbow Angle (delta_0) using Law of Cosines
+% We calculate the internal angle of the elbow when q=0
+dist_reach_0 = norm(p4_0 - p2_0); % Distance P2->P4 at rest
 
+% Law of Cosines: c^2 = a^2 + b^2 - 2ab*cos(angle)
+cos_delta_0 = (lambda_1^2 + lambda_2^2 - dist_reach_0^2) / (2 * lambda_1 * lambda_2);
+cos_delta_0 = min(1, max(-1, cos_delta_0)); 
+delta_0 = acos(cos_delta_0);
+
+% Calculate Beta (Shoulder offset angle at rest)
+beta = acos((vec_Upper_0' * frame_2_x_axis) / lambda_1);
+
+% Vector from Shoulder (P2) to Target Wrist
 p_2_to_wrist_worldFrame = world_frame_wrist_position - joint_2_pos_func(IK(1));
 lambda_3 = norm(p_2_to_wrist_worldFrame);
 
-cos_ratio = (lambda_1^2 + lambda_2^2 - lambda_3^2)/(2*lambda_1*lambda_2);
+% Calculate Required Elbow Angle (delta_now)
+cos_ratio = (lambda_1^2 + lambda_2^2 - lambda_3^2) / (2 * lambda_1 * lambda_2);
 cos_ratio_clamped = fmin(1, fmax(-1, cos_ratio));
 delta_now = acos(cos_ratio_clamped);
+
+% Solve Joint 3: The difference between the rest angle and required angle
 IK(3) = delta_0 - delta_now;
 
-D = norm(p_2_to_wrist_worldFrame(1:2));
-psi = atan2(p_2_to_wrist_worldFrame(3), D);
-phi = asin(lambda_2*sin(delta_now)/lambda_3);
-IK(2) = beta - (phi + psi);
+% Calculate the Shoulder Triangle Geometry at REST (q=0)
+vec_Shoulder_Wrist_0 = p4_0 - p2_0;
+
+% Elevation of the wrist at rest (psi_0)
+D_0 = norm(vec_Shoulder_Wrist_0(1:2));
+psi_0 = atan2(vec_Shoulder_Wrist_0(3), D_0);
+
+% Internal triangle angle at shoulder at rest (phi_0)
+cos_phi_0 = (lambda_1^2 + dist_reach_0^2 - lambda_2^2) / (2 * lambda_1 * dist_reach_0);
+cos_phi_0 = min(1, max(-1, cos_phi_0));
+phi_0 = acos(cos_phi_0);
+
+% Define the "Zero Offset" for Joint 2
+% This represents the total global angle of the upper arm when q2 = 0
+q2_offset = phi_0 + psi_0; 
+
+% Calculate the Shoulder Triangle Geometry for TARGET
+p_2_to_wrist = world_frame_wrist_position - joint_2_pos_func(IK(1));
+D_target = norm(p_2_to_wrist(1:2));
+psi_target = atan2(p_2_to_wrist(3), D_target);
+
+% Internal triangle angle at shoulder for target (phi_target)
+phi_target = asin(lambda_2 * sin(delta_now) / lambda_3);
+
+% Solve Joint 2
+IK(2) = q2_offset - (phi_target + psi_target);
 
 R_P3_to_world = world_to_wrist_func(IK(1:3));
 yaw_offset = pi/2;
