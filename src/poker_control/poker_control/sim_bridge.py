2896fe28669a55a4dffe62ad27bbe502a1f2e8b5
Faisal <faisallawan08@gmail.com>
2026-03-18 16:06:00 +0000

    Updated SERVO.ZERO in sim_bridge.py

--- src/poker_control/poker_control/sim_bridge.py
+++ src/poker_control/poker_control/sim_bridge.py
@@ -17,8 +17,9 @@ class SimBridge(Node):
         
         # --- Config ---
         self.n_joints = 6
-        self.joint_names = ['1', '2', '3', '4', '5', '6'] 
-        self.SERVO_ZERO = [2048] * self.n_joints
+        self.joint_names = ['1', '2', '3', '4', '5', '6']
+        # [2048] * self.n_joints
+        self.SERVO_ZERO = [2048, 2048, 479, 1873, 2048, 2048]
         self.SERVO_SIGNS = [1] * self.n_joints
         self.STEPS_PER_RAD = 4096.0 / (2.0 * np.pi)
 
